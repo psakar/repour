cd88c01233fd1938c904f51d16cbc033247ac33e
Petr Sakař <psakar@redhat.com>
2016-03-15 10:33:46 +0100

    allow to reuse cloned internal repo

--- test.sh
+++ test.sh
@@ -9,14 +9,9 @@ INTERNAL_REPO_URL=git://git.app.eng.bos.redhat.com/undertow-io/undertow.git
 INTERNAL_REPO=/tmp/undertow
 
 
-
-cleanup() {
-	rm -rf $REPOUR_REPO
-	rm -rf $INTERNAL_REPO
-}
-
 create_internal_repo() {	
 	echo "Cloning internal and upstream repo"
+	rm -rf $INTERNAL_REPO
 	mkdir $INTERNAL_REPO
 	cd $INTERNAL_REPO
 	git init
@@ -47,6 +42,7 @@ add_tag_to_repour_repo() {
 }
 
 create_repour_internal_repo() {
+	rm -rf $REPOUR_REPO
 	declare -a TAGS=("1.0.0.Final" "1.0.1.Final" "1.0.10.Final" "1.0.11.Final" "1.0.12.Final" "1.0.13.Final" "1.0.14.Final" "1.0.15.Final" "1.0.16.Final" "1.0.17.Final" "1.0.18.Final" "1.0.19.Final" "1.0.2.Final" "1.0.3.Final" "1.0.4.Final" "1.0.5.Final" "1.0.6.Final" "1.0.7.Final" "1.0.8.Final" "1.0.9.Final" "1.1.0.Final" "1.1.1.Final" "1.1.2.Final" "1.1.3.Final" "1.1.4.Final" "1.1.5.Final" "1.1.6.Final" "1.1.7.Final" "1.1.8.Final" "1.1.9.Final" "1.2.0.Final" "1.2.1.Final" "1.2.10.Final" "1.2.11.Final" "1.2.12.Final" "1.2.2.Final" "1.2.3.Final" "1.2.4.Final" "1.2.5.Final" "1.2.6.Final" "1.2.7.Final" "1.2.8.Final" "1.2.9.Final" "1.3.0.Final" "1.3.1.Final" "1.3.2.Final" "1.3.3.Final" "1.3.4.Final" "1.3.5.Final" "1.3.6.Final" "1.3.7.Final" "1.3.8.Final" "1.3.9.Final" "1.3.10.Final" "1.3.11.Final" "1.3.12.Final" "1.3.13.Final" "1.3.14.Final" "1.3.15.Final" "1.3.16.Final" "1.3.17.Final" "1.3.18.Final")
 	for TAG in "${TAGS[@]}"
 	do
@@ -90,7 +86,6 @@ apply_prod_changes_orig() {
 	done
 }
 
-cleanup
 create_internal_repo
 show_repo_info $INTERNAL_REPO
 create_repour_internal_repo
